b33d9be9e845bdd25ec9a84a99037d3e790ad763
Nahim Nasser <nnasser@gmail.com>
2016-02-12 17:44:43 -0500

    try different fonts

--- ledMatrix.py
+++ ledMatrix.py
@@ -1,9 +1,10 @@
 import max7219.led as led
+import max7219.font as font
 
 device = led.matrix(cascaded=2)
 device.orientation(270, redraw=True)
 
 def sendMessageToMatrix(message):
-    device.show_message(message)
+    device.show_message(message, font=font.LCD_FONT)
 
 sendMessageToMatrix("Server Booting...")
